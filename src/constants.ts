7ace0f0f5ab22d853a35cfc8461f71b0a2bb7cd3
Nick Anderson (XBOX) <niande@microsoft.com>
2020-04-18 01:45:35 -0700

    Added limits on battery (enforcing the range [0,100]). Moved to using os.cpuCurrentspeed().avg instead of getting the static speed. Hiding battery display altogether if the device doesn't have a battery.

---
+++ src/constants.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+export enum Units {
+    None = 1,
+    K = 1024,
+    M = 1024 * 1024,
+    G = 1024 * 1024 * 1024
+}
+
+export enum DiskSpaceFormat {
+    PercentUsed,
+    PercentRemaining,
+    Remaining,
+    UsedOutOfTotal
+}
+
+export interface DiskSpaceFormatLookup {
+    [unit: string]: DiskSpaceFormat;
+}
+
+export interface UnitLookup {
+    [unit: string]: number;
+}
+
+export var DiskSpaceFormatMappings: DiskSpaceFormatLookup = {
+    "PercentUsed": DiskSpaceFormat.PercentUsed,
+    "PercentRemaining": DiskSpaceFormat.PercentRemaining,
+    "Remaining": DiskSpaceFormat.Remaining,
+    "UsedOutOfTotal": DiskSpaceFormat.UsedOutOfTotal,
+};
+
+export var FreqMappings: UnitLookup = {
+    "GHz": Units.G,
+    "MHz": Units.M,
+    "KHz": Units.K,
+    "Hz": Units.None,
+};
+
+export var MemMappings: UnitLookup = {
+    "GB": Units.G,
+    "MB": Units.M,
+    "KB": Units.K,
+    "B": Units.None,
+};
